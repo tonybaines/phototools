ea27b90c893bd5eb1a517e6ef9a6af061e3cf649
tony <tony@count-zero>
2009-07-05 16:11:25 +0100

    savepoint

--- card2pc.sh
+++ card2pc.sh
@@ -7,21 +7,26 @@ function image_create_timestamp() {
     exiftool -d '%Y%m%d%H%M.%S' -p '$createDate' -q "$1"
 }
 
-SRC=$1
-DEST=$2
+
+if [ $# != 2 ]
+then
+    echo "Copy images from a camera card to destinations organised by shooting date"
+    echo "Usage: $0 SRC DEST"
+    exit 1
+else
+    SRC=$1
+    DEST=$2
+fi
 
 for f in `find $SRC -type f`
 do
     date=`image_create_date $f`
     create_timestamp=`image_create_timestamp $f`
     
-    
-    echo "Copying: $f to $DEST/$date/"
-    
     if [ ! -d $DEST/$date/ ]
     then
+        echo "Creating destination directory $DEST/$date" 
         mkdir -p $DEST/$date
     fi
-    cp "$f" "$DEST/$date/"
-    touch -ct $create_timestamp "$DEST/$date/$filename"
+    cp -upv -t "$DEST/$date/" "$f" 
 done 
